6317b8c8612c9ab8d7132030fd88eb5b510ac8e7
Khoi Nguyen <khoinm2003@gmail.com>
2025-05-26 23:14:05 +1000

    Feat: Install Jotai

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import './globals.css';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import { Inter } from 'next/font/google';
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+import { JotaiDevTools } from '../components/DevToolsClient';
+
+const inter = Inter({
   subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -23,12 +21,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <>
+      <JotaiDevTools />
+      <html lang="en">
+        <body className={`${inter.className} antialiased`}>{children}</body>
+      </html>
+    </>
   );
 }
---
+++ components/DevToolsClient.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import 'jotai-devtools/styles.css';
+
+import { DevTools } from 'jotai-devtools';
+
+export const JotaiDevTools = () => <DevTools theme="dark" />;
