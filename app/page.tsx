46b9a5bf3b049e3e0036810a8c980aa54954a658
Khoi Nguyen <khoinm2003@gmail.com>
2025-05-30 15:52:40 +1000

    Feat: Make loading skeletons for the home page
    Signed-off-by: Khoi Nguyen <khoinm2003@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import {MenuCategories} from '@/components/menu/menu-categories';
-import {MenuItems} from '@/components/menu/menu-items';
 import {MenuItem} from '@/domain/models/menu-item';
 import {MenuItemService} from '@/domain/services/menu-item-service';
 import {useQuery} from "@tanstack/react-query";
 import {container} from "@/infrastructure/container";
+import {Skeleton} from "@/components/ui/skeleton";
+import {Card, CardContent, CardTitle} from "@/components/ui/card";
+import {Button} from "@/components/ui/button";
+import {MenuCategories} from "@/components/menu/menu-categories";
+import {MenuItems} from "@/components/menu/menu-items";
 
-const useMenuItems = () => {
+export function useMenuItems() {
     const fetchMenuItems = (): Promise<MenuItem[]> => {
         const menuItemService = container.get<MenuItemService>("MenuItemService");
 
@@ -33,7 +36,7 @@ export default function Home() {
     const isLoading = menuItemsAreLoading;
     const isError = menuItemsAreError;
 
-    if (isLoading) return <div>Loading...</div>;
+    if (isLoading) return <HomePageLoadingSkeleton/>;
     if (isError) return <div>Error loading menu items</div>;
 
     return (<div className="flex flex-1 flex-col gap-5 items-start justify-start">
@@ -41,3 +44,63 @@ export default function Home() {
         <MenuItems menuItems={menuItems || []}/>
     </div>);
 };
+
+const HomePageLoadingSkeleton = () => {
+    const MenuCategoriesSkeleton = () => {
+        const menuCategories = ["All", "Drinks", "Breakfast", "Lunch", "Dinner"]
+
+        return (<div className="flex flex-row gap-2.5 w-full">
+            {menuCategories.map((menuCategory) => (
+                <Skeleton key={menuCategory}>
+                    <Card className="px-5 bg-inherit text-accent shadow-none border-none">
+                        <CardTitle>{menuCategory}</CardTitle>
+                    </Card>
+                </Skeleton>
+            ))}
+        </div>)
+    }
+
+    const MenuItemsSkeleton = () => {
+        const menuItems: MenuItem[] = [
+            {
+                menuCategoryId: 0,
+                menuItemId: 0,
+                name: 'menuItem1',
+                price: 0
+            },
+            {
+                menuCategoryId: 0,
+                menuItemId: 1,
+                name: 'menuItem2',
+                price: 0
+            },
+            {
+                menuCategoryId: 0,
+                menuItemId: 2,
+                name: 'menuItem3',
+                price: 0
+            },
+        ]
+
+        return (<div className="flex flex-row gap-2.5 w-full">
+            {menuItems.map((menuItem) => (
+                <Skeleton key={menuItem.menuItemId}>
+                    <Card className="p-2.5 bg-inherit text-accent shadow-none border-none items-center flex-row">
+                        <div className="flex flex-col gap-2.5">
+                            <CardTitle>{menuItem.name}</CardTitle>
+                            <CardContent className="p-0">
+                                <span>{menuItem.price}</span>
+                            </CardContent>
+                        </div>
+                        <Button className="ml-auto border-none bg-inherit shadow-none" variant="outline" size="icon"/>
+                    </Card>
+                </Skeleton>
+            ))}
+        </div>)
+    }
+
+    return <div className="flex flex-1 flex-col gap-5 items-start justify-start">
+        <MenuCategoriesSkeleton/>
+        <MenuItemsSkeleton/>
+    </div>;
+}
--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,12 +1,12 @@
 import {cn} from "@/lib/utils"
 import {ComponentProps} from "react";
 
-function Skeleton({className, ...props}: ComponentProps<"div">) {
+function Skeleton({className, children, ...props}: ComponentProps<"div">) {
     return (<div
         data-slot="skeleton"
         className={cn("bg-accent animate-pulse rounded-md", className)}
         {...props}
-    />)
+    >{children}</div>)
 }
 
 export {Skeleton}
