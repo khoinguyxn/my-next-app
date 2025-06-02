8c6b98a30ece1147853977b46480cd7f5c92f6fe
Khoi Nguyen <khoinm2003@gmail.com>
2025-06-02 17:53:13 +0700

    Feat: Build left Sheet opening when finalizing ordering qty
    Signed-off-by: Khoi Nguyen <khoinm2003@gmail.com>

--- components/menu/menu-items.tsx
+++ components/menu/menu-items.tsx
@@ -7,6 +7,7 @@ import {Card, CardContent, CardTitle} from '../ui/card';
 import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
 import {Label} from "@/components/ui/label";
 import {useState} from "react";
+import {Sheet, SheetContent, SheetHeader, SheetTrigger} from "@/components/ui/sheet";
 
 export const MenuItems = ({menuItems}: { menuItems: MenuItem[] }) => {
     return (<div
@@ -20,37 +21,47 @@ export const MenuItems = ({menuItems}: { menuItems: MenuItem[] }) => {
 const MenuItemCard = ({menuItem}: { menuItem: MenuItem }) => {
     const [menuItemQuantity, setMenuItemQuantity] = useState(1);
 
-    return (<Card className="flex-row p-2.5 items-center">
-        <div className="flex flex-col gap-2.5">
-            <CardTitle>{menuItem.name}</CardTitle>
-            <CardContent className="p-0">
-                <span>{menuItem.price.toFixed(2)}</span>
-            </CardContent>
-        </div>
-        <Popover>
-            <PopoverTrigger asChild>
-                <Button className="ml-auto" variant="outline" size="icon">
-                    <PlusIcon/>
-                </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-fit">
-                <div className="flex flex-col gap-2.5">
-                    <div className="flex flex-row items-center gap-2.5">
-                        <Button variant="outline" size="icon" onClick={() => setMenuItemQuantity(menuItemQuantity - 1)}
-                                disabled={menuItemQuantity === 1}>
-                            <MinusIcon/>
-                        </Button>
-                        <Label htmlFor="menuItemQuantity">{menuItemQuantity}</Label>
-                        <Button variant="outline" size="icon" onClick={() => setMenuItemQuantity(menuItemQuantity + 1)}
-                                disabled={menuItemQuantity === 10}>
-                            <PlusIcon/>
-                        </Button>
-                    </div>
-                    <Button>
-                        <Label htmlFor="orderWithQuantity">Order</Label>
+    return (<Sheet>
+        <Card className="flex-row p-2.5 items-center">
+            <div className="flex flex-col gap-2.5">
+                <CardTitle>{menuItem.name}</CardTitle>
+                <CardContent className="p-0">
+                    <span>{menuItem.price.toFixed(2)}</span>
+                </CardContent>
+            </div>
+            <Popover>
+                <PopoverTrigger asChild>
+                    <Button className="ml-auto" variant="outline" size="icon">
+                        <PlusIcon/>
                     </Button>
-                </div>
-            </PopoverContent>
-        </Popover>
-    </Card>);
+                </PopoverTrigger>
+                <PopoverContent className="w-fit">
+                    <div className="flex flex-col gap-2.5">
+                        <div className="flex flex-row items-center gap-2.5">
+                            <Button variant="outline" size="icon"
+                                    onClick={() => setMenuItemQuantity(menuItemQuantity - 1)}
+                                    disabled={menuItemQuantity === 1}>
+                                <MinusIcon/>
+                            </Button>
+                            <Label htmlFor="menuItemQuantity">{menuItemQuantity}</Label>
+                            <Button variant="outline" size="icon"
+                                    onClick={() => setMenuItemQuantity(menuItemQuantity + 1)}
+                                    disabled={menuItemQuantity === 10}>
+                                <PlusIcon/>
+                            </Button>
+                        </div>
+                        <SheetTrigger asChild>
+                            <Button>
+                                <Label htmlFor="orderWithQuantity">Order</Label>
+                            </Button>
+                        </SheetTrigger>
+                    </div>
+                </PopoverContent>
+            </Popover>
+        </Card>
+
+        <SheetContent className="rounded-l-lg">
+            <SheetHeader>Your order</SheetHeader>
+        </SheetContent>
+    </Sheet>);
 };
