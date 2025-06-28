7338981c18dc81cff6ce5880b677fde8446dd4da
Khoi Nguyen <khoinm2003@gmail.com>
2025-06-28 13:13:21 +1000

    Feat: Build OrderItemService's getAll method
    Signed-off-by: Khoi Nguyen <khoinm2003@gmail.com>

--- domain/services/order-item-service.ts
+++ domain/services/order-item-service.ts
@@ -1,9 +1,14 @@
 import "reflect-metadata";
 import { inject, injectable } from "inversify";
 import type { IOrderItemRepository } from "@/domain/repositories/i-order-item-repository";
-import { OrderItemWithInsert } from "@/domain/models/orders/order-item";
+import {
+  OrderItem,
+  OrderItemWithInsert,
+} from "@/domain/models/orders/order-item";
 
 export interface IOrderItemService {
+  getAll(orderNumber: number): Promise<OrderItem[]>;
+
   create(orderItems: OrderItemWithInsert[]): Promise<void>;
 }
 
@@ -13,6 +18,10 @@ export class OrderItemService implements IOrderItemService {
     @inject("OrderItemRepository")
     private orderItemRepository: IOrderItemRepository,
   ) {}
+
+  async getAll(orderNumber: number): Promise<OrderItem[]> {
+    return await this.orderItemRepository.getAll(orderNumber);
+  }
 
   async create(orderItems: OrderItemWithInsert[]): Promise<void> {
     await this.orderItemRepository.create(orderItems);
